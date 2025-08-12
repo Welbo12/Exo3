e6313fb950861056c38a529330380de198888d5f
welborn <djidjonouaflou@gmail.com>
2025-08-12 15:42:52 +0000

    avant dernier modife

--- mon-projet/src/components/SixiemePage/SixPrincipale.tsx
+++ mon-projet/src/components/SixiemePage/SixPrincipale.tsx
@@ -1,10 +1,64 @@
+// import React from 'react'
+// import But from '../Petitcomposant/But'
+
+// const SixPrincipale:React.FC=()=> (
+//     <div className='but' >
+//         <But label="contact" />
+//         <h1>
+//             Let’s Discuss<span className="highlight-green"> Your Project </span>
+//         </h1>
+//         <p>
+//             Let’s make something new, different
+//             and more meaningful or make thing more visual or conceptual
+//         </p>
+
+//     </div>
+// )
+
+// export default SixPrincipale
+
+
+
 import React from 'react'
 import But from '../Petitcomposant/But'
 
-const SixPrincipale:React.FC=()=> {
+const SixPrincipale: React.FC = () => {
+  const containerStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    textAlign: 'center',
+    padding: '50px',
+    boxSizing: 'border-box',
+  }
+
+  const headingStyle: React.CSSProperties = {
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    marginTop: '20px',
+  }
+
+  const paragraphStyle: React.CSSProperties = {
+    maxWidth: '600px',
+    marginTop: '15px',
+    fontSize: '16px',
+    lineHeight: '1.6',
+  }
+
+  const highlightGreenStyle: React.CSSProperties = {
+    color: '#0DB760',
+  }
+
   return (
-    <div>
-        <But label="contact" className='but' />
+    <div style={containerStyle}>
+      <But label="Contact" onClick={() => alert('Clicked')} />
+      <h1 style={headingStyle}>
+        Let’s Discuss <span style={highlightGreenStyle}>Your Project</span>
+      </h1>
+      <p style={paragraphStyle}>
+        Let’s make something new, different and more meaningful or make things more visual or conceptual.
+      </p>
     </div>
   )
 }
